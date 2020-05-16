fcb7667a18632d88dca074333635af34986ae000
rozay10 <raadeniyi3@gmail.com>
2020-05-16 17:25:15 +0100

    refactored authcontroller and fixed codacy issue

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,8 @@
+import database from '../models/index';
 import ResponseGenerator from '../utils/response.utilities';
-import UserService from '../services/user.service';
-import sendEmail from '../utils/email.utilities';
-
+import GeneralUtils from '../utils/general.utilities';
+import Auth from '../middlewares/auth.middleware';
+import sendEmail from '../services/email.service';
 
 const response = new ResponseGenerator();
 
@@ -15,24 +16,35 @@ class AuthController {
 
   static async signup(req, res) {
     try {
-      const user = await UserService.addUser(req, res);
-      if (user.email) {
+      const { email } = req.body;
+      const foundUser = await database.User.findOne({
+        where: { email }
+      });
+      if (foundUser) {
+        res.status(400).json({
+          status: 'error',
+          message: 'Email is already in use'
+        });
+      }
+      req.body.password = await GeneralUtils.hash(req.body.password);
+      const newUser = await database.User.create(req.body);
+      const result = newUser.toJSON();
+      const token = Auth.signJwt(result);
+      delete result.password;
+      if (result.email) {
         // 3) Send verification link  to user's email
-        const verificationURL = `${req.protocol}://${req.get(
-          'host'
-        )}/api/v1/auth/verification/${user.token}`;
+        const verificationURL = `${req.protocol}://${req.get('host')}/api/v1/auth/verification/${token}`;
 
         const message = ` Click on this link to verify your account: ${verificationURL}`;
         await sendEmail({
-          email: user.email,
+          email: result.email,
           subject: 'Your link only (valid for some mins)',
           message
         });
-        return response.sendSuccess(
-          res,
-          201,
-          user
-        );
+        return response.sendSuccess(res, 201, {
+          token,
+          ...result
+        });
       }
       return response.sendError(res, 500, 'Something went wrong');
     } catch (err) {
@@ -40,28 +52,29 @@ class AuthController {
     }
   }
 
-  /**
-   * @param {object} request express request object
-   * @param {object} response express request object
-   * @returns {json} json
-   * @memberof UserController
+  /** verify User by email
+   * @description Operate on a user and his account
+   * @param {object} user object is updated
    */
 
   static async isVerified(req, res) {
     try {
-      const verified = await UserService.isVerified(req);
-      if (verified) {
+      const { token } = req.params;
+      const decoded = Auth.decodeJwt(token);
+      if (decoded) {
+        const { id } = decoded;
+        await database.User.update({ isVerified: true }, { where: { id } });
         return res.status(200).json({
           status: 'success',
           message: 'your account has been verified, Welcome'
         });
       }
+
       return response.sendError(res, 500, 'Your token is wrong or has expired try again later!');
     } catch (error) {
       return response.sendError(res, 500, error.message);
     }
   }
 }
-
 
 export default AuthController;
