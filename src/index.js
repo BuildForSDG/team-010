9175d634f1215db396707e64f804a45fbfd8d199
rozay10 <raadeniyi3@gmail.com>
2020-05-22 13:37:06 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
-// import app from './app';
-
-// const startApp = async () => {
-//   const header = document.querySelector('[data-app-name]');
-//   if (!header) return;
-
-//   const programName = await app();
-//   header.textContent = programName;
-// };
-
-// document.addEventListener('DOMContentLoaded', startApp);
 import winston from 'winston';
-
 import app from './app';
+import keys from './utils/config.utilities';
 
+
+<<<<<<< HEAD
 const port = process.env.PORT || 3000;
+=======
+const port = keys.port || 3000;
+>>>>>>> refactor
 const logger = winston.createLogger({
   transports: [
     new winston.transports.Console()
