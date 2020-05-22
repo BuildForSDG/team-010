6f8f170fc2dd1986b436e64df4d1a558d923129f
rozay10 <raadeniyi3@gmail.com>
2020-05-22 13:37:06 +0100

    refactor

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,3 @@
-
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
@@ -14,34 +13,23 @@ const config = configJson[env];
 const db = {};
 
 let sequelize;
-if (config.environment === 'production') {
-  sequelize = new Sequelize(
-    process.env[config.use_env_variable], config
-  );
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS, {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-      dialectOption: {
-        ssl: true,
-        native: true
-      },
-      logging: true
-    }
-  );
+if (env === 'production') {
+  sequelize = new Sequelize(process.env.DB_NAME_PROD, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOption: {
+      ssl: true,
+      native: true
+    },
+    logging: false
+  });
 } else {
-  sequelize = new Sequelize(
-    config.database, config.username, config.password, config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter((file) => (file.indexOf('.') !== 0)
-           && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(__dirname)
+  .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
