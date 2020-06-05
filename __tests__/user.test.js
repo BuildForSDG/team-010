79d9c709f3eb1db438e27ea233f549fb9f911123
Amaitari Hezekiah Philip <amaitarihezekiah@gmail.com>
2020-06-05 11:47:34 +0100

    refactor(test): refactor test files

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,16 +2,14 @@ import request from 'supertest';
 import app from '../src/app';
 import database from '../src/models/index';
 
-
 /**
    * Test User Endpoints
    */
 describe('{GET/POST} Test user signup/signin', () => {
 /**
-   * Test the POST /auth/signup endpoint
+   * POST /auth/signup endpoint
    */
-
-  test('Should signup a new user', async () => {
+  it('Should signup a new user', async (done) => {
     const response = await request(app).post('/api/v1/auth/signup').send({
       firstName: 'Rasak',
       middleName: 'Agbolade',
@@ -25,7 +23,6 @@ describe('{GET/POST} Test user signup/signin', () => {
     }).expect(201);
 
     const { body: { data } } = response;
-    // console.log(data);
 
     // Assert that the database was changed correctly
     const { dataValues } = await database.User.findOne({ where: { email: data.email } });
@@ -40,14 +37,16 @@ describe('{GET/POST} Test user signup/signin', () => {
     expect(dataValues).toHaveProperty('email', 'raadeniyi22@gmail.com');
     expect(dataValues).toHaveProperty('phone', '7031059595');
     expect(dataValues.password).not.toBe('unencryptedPassword');
+    done();
   });
 
-  // test('Should login existing user', async () => {
+  // it('Should login existing user', async (done) => {
   //   const response = await request(app).post('/api/v1/auth/signin').send({
   //     email: 'raadeniyi22@gmail.com',
   //     password: 'test1234'
   //   }).expect(200);
-  //   const user = await database.User.findOne({ where: { email: 'raadeniyi22@gmail.com' } });
+
   //   expect(response.body).toHaveProperty('token');
+  //   done();
   // });
 });
